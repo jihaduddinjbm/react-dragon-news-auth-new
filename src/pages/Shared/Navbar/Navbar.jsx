afb043655c7867ea80882e52d2a933531b46b3a3
jihaduddinjbm <jihaduddin117@gmail.com>
2023-10-31 22:10:40 +0600

    added all jsx file

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -7,6 +7,8 @@ const Navbar = () => {
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/career">Career</NavLink></li>
+        <li><NavLink to="/login">login</NavLink></li>
+
 
     </>
 
